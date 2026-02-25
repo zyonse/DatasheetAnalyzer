39389c2dc1a4f3439e059de5e7d785008165325d
Gavin Zyonse <gavin@zyonse.com>
2026-02-25 16:14:39 -0500

    Initial Commit

---
+++ DatasheetAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatasheetAnalyzer
+//
+//  Created by Gavin Zyonse on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatasheetAnalyzer/DatasheetAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatasheetAnalyzerApp.swift
+//  DatasheetAnalyzer
+//
+//  Created by Gavin Zyonse on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct DatasheetAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
